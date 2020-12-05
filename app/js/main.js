27824043c7b2c16bb07c50cc8e7fce565bc334e6
hollywood <hollywood@MacBook-Pro-hollywood.local>
2020-12-05 17:19:03 +0300

    added testimonials, campaigns

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
 
+    $('.testimonials__slider').slick({
+        dots: false,
+        infinite: false,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+      });
+
+
 });
